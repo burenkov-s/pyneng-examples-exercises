550b89c9d93c4b4066088f815aa554305d1e0c11
burenkov-s <burenkov-sr@yandex.ru>
2021-05-15 17:23:29 +0700

    solved

--- exercises/04_data_structures/task_4_4.py
+++ exercises/04_data_structures/task_4_4.py
@@ -24,3 +24,9 @@
 """
 
 vlans = [10, 20, 30, 1, 2, 100, 10, 30, 3, 4, 10]
+vlans.sort()
+result = []
+for element in vlans:
+    if element not in result:
+        result.append(element)
+print(result)
