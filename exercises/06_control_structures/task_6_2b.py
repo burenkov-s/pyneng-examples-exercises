641263f70b345e2a6a790bcae173b306aa9be341
burenkov-s <burenkov-sr@yandex.ru>
2022-02-18 16:22:19 +0700

    fixed mistake

--- exercises/06_control_structures/task_6_2b.py
+++ exercises/06_control_structures/task_6_2b.py
@@ -8,3 +8,33 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+ip_is_correct = False
+while True:
+    ip_is_correct = True
+    ip = input("Введите IP адрес:")
+    octets = ip.split(".")
+    if len(octets) == 4:
+        for octet in octets:
+            if int(octet) < 0 or int(octet) > 255:
+                ip_is_correct = False
+                break
+    else:
+        ip_is_correct = False
+    if ip_is_correct:
+        break
+    else:
+        print("IP is not correct. Try again")
+
+if ip_is_correct:
+    if int(ip.split(".")[0]) in range(1, 223):
+        print("unicast")
+    elif int(ip.split(".")[0]) in range(224, 239):
+        print("multicast")
+    elif ip == "255.255.255.255":
+        print("local broadcast")
+    elif ip == "0.0.0.0":
+        print("unassigned")
+    else:
+        print("unused")
+else:
+    print("ip is not correct")
